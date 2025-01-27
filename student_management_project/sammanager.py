11e5d0ea977efe4c7912866e3d161179f7374467
30178812 <147609790+30178812@users.noreply.github.com>
2025-01-27 15:30:19 +0000

    Docstrings

--- student_management_project/sammanager.py
+++ student_management_project/sammanager.py
@@ -1,8 +1,10 @@
+""""""
 # List to hold student records
 students = []
 
 # Function to add a new student record with name, age, and marks
 def add_student():
+    """"""
     name = input("\nEnter student name: ")
     age = int(input("Enter student age: "))
     marks = int(input("Enter the amount of marks (1-100): "))
@@ -13,6 +15,7 @@ def add_student():
 
 # Function to display all student records
 def display_students():
+    """"""
     if students:
         print("\nStudent Records:")
         for student in students:
@@ -22,6 +25,7 @@ def display_students():
 
 # Function to delete a student record by name
 def delete_student():
+    """"""
     name = input("\nEnter student to be deleted: ")
 
     global students
@@ -29,19 +33,15 @@ def delete_student():
     print(f"Student with name {name} has been deleted.")
 
 def main():
+    """"""
     while True:
         print("\n1. Add student")
         print("2. Display student")
         print("3. Delete student")
-
         user = input("Enter 1-3: ")
-
         match user:
             case "1": add_student()
             case "2": display_students()
             case "3": delete_student()
 
-
 main()
-
-

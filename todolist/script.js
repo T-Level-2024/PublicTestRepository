9c88cba641e3dbc9a7ca998cbf7a01a5aabf376b
30178812 <147609790+30178812@users.noreply.github.com>
2025-03-06 15:26:28 +0000

    To-do list

---
+++ todolist/script.js
@@ -0,0 +1,97 @@
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+const addbutton = document.getElementById("add");
+const removebutton = document.getElementById("remove");
+let tasks = [];
+
+// tasks array
+// new task is aded to this array
+// you through this array, then for loop to make li elements for each task
+// you can make an edit feature
+// you can go through css
+// deploy this
+
+// make a function calle updateTaskList()
+
+list.addEventListener("click", (event) => {
+    // Checks what the element ID ends with:
+    // R for remove
+    // E for edit
+    // S for submit (comes up after edit is pressed)
+    let target = event.target;
+    let element = target.id;
+    let index = element.charAt(1);
+    if (element.endsWith("R")) {
+        removeItem(index);
+    } else if (element.endsWith("E")) {
+        enableEditing(index);
+    } else if (element.endsWith("S")) {
+        replaceItem(index, document.getElementById(`L${index}I`).value);
+    };
+});
+
+function updateTaskList() {
+    console.log("updateTaskList() called")
+    list.innerHTML = "";
+    let index;
+    if (tasks.length === 0) {
+        list.innerHTML += "<li>No things to do.</li>";
+    };
+    for (let i=0; i<tasks.length; i++) {
+        index = i;
+        //console.log(`updateTaskList(): I(${index}) task(${tasks[index]})`);
+        if(tasks[i][1]){ 
+            // Input box, submit, remove
+            // Any existing text is moved to the input box.
+            list.innerHTML += `<li id="L${index}"><input id="L${index}I" type="text" placeholder="New text here" value="${tasks[index][0]}"><button id="L${index}S">Submit</button><button id="L${index}R">Remove</button></li>`;
+        } else { 
+            // Item, edit, remove
+            // User can either edit the item text or remove the item entirely
+            list.innerHTML += `<li id="L${i}">${tasks[index][0]} <button id="L${index}E">Edit</button><button id="L${index}R">Remove</button></li>`;
+        };
+    };
+};
+
+function enableEditing(element) {
+    console.log(`Now editing: L${element}`)
+    tasks[element][1] = true;
+    updateTaskList();
+}
+
+// submit button functiojn
+// print to the console 
+
+function replaceItem(element, text) {
+    if(text.trim == "") {
+        return;
+    };
+    console.log(`L${element}: ${tasks[element][0]} --> ${text}`);
+    tasks[element][0] = text;
+    tasks[element][1] = false;
+    updateTaskList();
+}
+
+function removeItem(element) {
+    console.log(`Removing L${element} (${tasks[element][0]})`)
+    tasks.splice(element, 1);
+    updateTaskList();
+}
+
+addbutton.addEventListener("click", () => {
+    if(input.value == "") {
+        return;
+    };
+    console.log(`New item: L${(tasks.length)} (${input.value})`)
+    tasks.push([input.value, false]);
+    input.value = "";
+    updateTaskList();
+});
+
+removebutton.addEventListener("click", () => {
+    //list.removeChild(list.lastElementChild);
+    console.log("All items removed")
+    list.innerHTML = ""
+    tasks = [];
+});
+
+updateTaskList();
